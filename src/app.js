6c567e015590feed765a345db53fe271af7aed46
MohitCode17 <mohit.codes17@gmail.com>
2024-06-15 12:37:19 +0530

    add body parse middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+// MIDDLEWARES
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // TEST API
 app.get("/api/test", (req, res) => {
   res.send("Test Passed !");
