c11f2baba448419d9f0990eb8dd6bd627a7111cf
MohitCode17 <mohit.codes17@gmail.com>
2024-06-15 17:58:20 +0530

    add delete message controller

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -24,9 +24,19 @@ export const handleSendMessage = catchAsyncErrors(async (req, res, next) => {
 });
 
 // DELETE MESSAGE CONTROLLER
-export const handleDeleteMessage = catchAsyncErrors(
-  async (req, res, next) => {}
-);
+export const handleDeleteMessage = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+
+  const message = await Message.findById(id);
+
+  if (!message) return next(new ErrorHandler("Message already deleted !", 400));
+
+  await message.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Message Deleted",
+  });
+});
 
 // GET MESSAGES CONTROLLER
 export const handleGetMessages = catchAsyncErrors(async (req, res, next) => {
