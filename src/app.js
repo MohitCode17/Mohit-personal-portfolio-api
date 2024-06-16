8af327257317bcf42d417fcdd415c209544a014d
MohitCode17 <mohit.codes17@gmail.com>
2024-06-16 12:06:57 +0530

    add cloudinary & upload file config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { errorMiddleware } from "./middleware/error.js";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// FILE UPLOAD MIDDLEWARE
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 // IMPORTS ROUTES
 import messageRoutes from "./routes/message.route.js";
--- src/config/env-config.js
+++ src/config/env-config.js
@@ -4,6 +4,9 @@ conf();
 const _config = {
   port: process.env.PORT,
   mongodb_uri: process.env.MONGODB_URI,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
