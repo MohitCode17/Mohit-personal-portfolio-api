a13dcae4702d15ca0b0b7a968c3e653c81c681cd
MohitCode17 <mohit.codes17@gmail.com>
2024-06-16 18:05:49 +0530

    add authenticate middleware to message route

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -4,13 +4,14 @@ import {
   handleGetMessages,
   handleSendMessage,
 } from "../controllers/message.controller.js";
+import { authenticate } from "../middleware/authenticate.js";
 
 const router = express.Router();
 
 // ROUTE: GET MESSAGES
 // PATH: /api/v1/message
 // METHOD: GET
-router.get("/", handleGetMessages);
+router.get("/", authenticate, handleGetMessages);
 
 // ROUTE: SEND MESSAGE
 // PATH: /api/v1/message/send
@@ -20,6 +21,6 @@ router.post("/send", handleSendMessage);
 // ROUTE: DELETE MESSAGE
 // PATH: /api/v1/message/delete/:id
 // METHOD: DELETE
-router.delete("/delete/:id", handleDeleteMessage);
+router.delete("/delete/:id", authenticate, handleDeleteMessage);
 
 export default router;
