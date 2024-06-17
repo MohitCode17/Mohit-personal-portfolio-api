7944633c9992bf152006fdd9b6d6c6a479c8cfe2
MohitCode17 <mohit.codes17@gmail.com>
2024-06-17 21:16:30 +0530

    add timeline model

---
+++ src/models/timeline.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const timelineSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Timeline title is required!"],
+    },
+    description: {
+      type: String,
+      required: [true, "Timeline description is required!"],
+    },
+    timeline: {
+      from: {
+        type: String,
+      },
+      to: {
+        type: String,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const Timeline = mongoose.model("Timeline", timelineSchema);
+
+export { Timeline };
