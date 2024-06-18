1315220399e98e105a7264266153da4eaef49f6f
MohitCode17 <mohit.codes17@gmail.com>
2024-06-18 18:19:37 +0530

    add skill model

---
+++ src/models/skill.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const skillSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  proficiency: {
+    type: Number,
+  },
+  svg: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+});
+
+const Skill = mongoose.model("Skill", skillSchema);
+
+export { Skill };
