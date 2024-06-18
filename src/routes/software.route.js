ae96a65327866e1805eb357845348ebf231ed922
MohitCode17 <mohit.codes17@gmail.com>
2024-06-18 17:30:42 +0530

    add delete software controller and route

--- src/routes/software.route.js
+++ src/routes/software.route.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { authenticate } from "../middleware/authenticate.js";
-import { handleAddSoftware } from "../controllers/softwre.controller.js";
+import {
+  handleAddSoftware,
+  handleDeleteSoftware,
+  handleGetSoftware,
+} from "../controllers/softwre.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +12,15 @@ const router = express.Router();
 // PATH: /api/v1/software/add
 // METHOD: POST
 router.post("/add", authenticate, handleAddSoftware);
+
+// ROUTE: GET ALL SOFTWARE
+// PATH: /api/v1/software/getall
+// METHOD: GET
+router.get("/getall", handleGetSoftware);
+
+// ROUTE: DELETE SOFTWARE
+// PATH: /api/v1/software/delete/:id
+// METHOD: DELETE
+router.delete("/delete/:id", handleDeleteSoftware);
 
 export default router;
